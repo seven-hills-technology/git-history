61f9ace4192542cc61d1181da48618ab347caa70
Spencer Dangel <dangelspencer@gmail.com>
2018-11-15 11:54:15 -0500

    Add --all to command fetching git history

--- filterGitHistory.js
+++ filterGitHistory.js
@@ -1,6 +1,7 @@
 const argv = require("./arguments");
 
 function filterGitHistory(line) {
+    if (line == null) return false;
     if (argv.author != null && !Array.isArray(argv.author) && line.author !== argv.author) return false;
     if (argv.author != null && Array.isArray(argv.author) && !argv.author.includes(line.author)) return false;
     if (argv.startDate != null && argv.startDate.diff(line.date) >= 0) return false;
